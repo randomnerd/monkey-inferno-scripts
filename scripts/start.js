095a8805f9a4bb16ab2fe4de1b5e350faf629168
randomnerd <someguy4112@gmail.com>
2017-04-07 23:50:54 +0300

    Update start.js

--- scripts/start.js
+++ scripts/start.js
@@ -6,12 +6,12 @@ process.env.NODE_ENV = 'development';
 require('dotenv').config({silent: true});
 
 var chalk = require('chalk');
-var appPath = require('react-scripts/config/paths').appPath;
+var appPath = require('inferno-scripts/config/paths').appPath;
 var webpackMonkeyPath = path.resolve(appPath, 'webpack.monkey.js');
-var webpackConfig = require('react-scripts/config/webpack.config.dev');
+var webpackConfig = require('inferno-scripts/config/webpack.config.dev');
 
 if (fs.existsSync(webpackMonkeyPath)) {
     console.log(chalk.yellow('WARNING! You are using modified webpack config!'));
     require(webpackMonkeyPath)(webpackConfig, true);
 }
-require('react-scripts/scripts/start');
+require('inferno-scripts/scripts/start');
